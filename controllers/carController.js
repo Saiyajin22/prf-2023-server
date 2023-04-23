769c2062fbee6c71d01033d8be8e4db4dd940526
tothz-novin <117072929+tothz-novin@users.noreply.github.com>
2023-04-23 15:36:00 +0200

    car controller crud continued

--- controllers/carController.js
+++ controllers/carController.js
@@ -13,4 +13,43 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.id);
+    if (car == null) {
+      res.status(404).json({ message: "No car exists with the given id" });
+    } else {
+      res.status(200).json(car);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:brand", async (req, res) => {
+  try {
+    const bmwCars = await Car.find({ brand: req.params.brand });
+    console.log("BMW Cars from db: ", bmwCars);
+    res.status(200).json(bmwCars);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const car = new Car({
+    brand: req.body.brand,
+    series: req.body.series,
+    bodyStyle: req.body.bodyStyle,
+    dateOfManufacturing: req.body.dateOfManufacturing,
+  });
+
+  try {
+    const newCar = await car.save();
+    res.status(201).json(newCar);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 module.exports = router;
