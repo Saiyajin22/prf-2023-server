a8a4bc5a48d50da7fb076c24e77868c9998e49bd
saiyajin22 <tothzoltan0713@gmail.com>
2023-05-05 15:55:30 +0200

    bicycle schema

---
+++ db/bicycleSchema.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const bicycleSchema = new mongoose.Schema({
+  brand: {
+    type: String,
+    required: true,
+  },
+  series: {
+    type: String,
+    required: true,
+  },
+  bodyStyle: {
+    type: String,
+    required: true,
+  },
+  frameMaterial: {
+    type: String,
+    required: true,
+  },
+  gearType: {
+    type: String,
+    required: false,
+  },
+  wheelSize: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+bicycleSchema.pre('save', function(next) {
+  const bicycle = this;
+  if(bicycle.price > 500000){
+    console.log("The most expensive bicycle costs 500.000$. Please lowert your price");
+    return next(error);
+  }
+
+  return next();
+});
+
+bicycleSchema.pre('find', function(next) {
+    const bicycle = this;
+    bicycle.where({price: { $lt: 500000 }})
+});
+
+const Bicycle = mongoose.model('bicycle', bicycleSchema);
+module.exports = Bicycle;
